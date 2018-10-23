2068be0d93872a09575b59a18e01ec801a326918
fabrezi <fkschon@gmail.com>
2018-10-23 15:22:43 -0400

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,18 +1,34 @@
-#it reads the file. form word frequency relation.
-
 import re
 import string
+import pandas as pd
+
+stop_words = [
+"a", "about", "above", "across", "after", "afterwards",
+"again", "all", "almost", "alone", "along", "already", "also",
+"although", "always", "am", "among", "amongst", "amoungst", "amount", "an",
+"and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "as", "at", "be", "became",
+"because", "become","becomes", "becoming", "been", "before", "behind", "being", "beside", "besides", "between",
+"beyond", "both", "but", "by","can", "cannot", "cant", "could", "couldnt", "de", "describe", "do", "done", "each",
+"eg", "either", "else", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "find","for",
+"found", "four", "from", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein",
+"hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "i", "ie", "if", "in", "indeed", "is", "it", "its", "itself", "keep", "least",
+"less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mine", "more", "moreover", "most", "mostly", "much", "must", "my", "myself", "name",
+"namely", "neither", "never", "nevertheless", "next","no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often",
+"on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part","perhaps", "please",
+"put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "she", "should","since", "sincere","so", "some", "somehow", "someone",
+"something", "sometime", "sometimes", "somewhere", "still", "such", "take","than", "that", "the", "their", "them", "themselves", "then", "thence", "there"
+"thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they",
+"this", "those", "though", "through", "throughout",
+"thru", "thus", "to", "together", "too", "toward", "towards",
+"under", "until", "up", "upon", "us",
+"very", "was", "we", "well", "were", "what", "whatever", "when",
+"whence", "whenever", "where", "whereafter", "whereas", "whereby",
+"wherein", "whereupon", "wherever", "whether", "which", "while",
+"who", "whoever", "whom", "whose", "why", "will", "with",
+"within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves"
+]
 
 frequency = {}
-document_text = open('flop', 'r')
-text_string = document_text.read().lower()
-match_pattern = re.findall(r'\b[a-z]{4,15}\b', text_string)
-
-for word in match_pattern:
-    count = frequency.get(word, 0)
-    frequency[word] = count + 1
-
-frequency_list = frequency.keys()
-
-for words in frequency_list:
-    print (words, frequency[words])
+news = pd.read_excel('C:\\Users\\farid-PC\\Desktop\\Tester.xlsx')
+pd.set_option('display.max_colwidth', 1000)
+print(news)
